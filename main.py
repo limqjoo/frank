bc66a825bcf9ac60e41485672a8d6de8a9f770d2
Tom Chambers <tom.chambers@gmail.com>
2017-04-22 18:05:18 +0100

    reorganise

--- main.py
+++ main.py
@@ -33,28 +33,41 @@ def sms_receiver():
 
     if (stage==0): # signing up players
         from_number = request.values.get('From', None)
-        characters[from_number] = {}
-        resp = MessagingResponse().message("Thanks for joining the game player number " + str(player_count+1) + " Please send your name.")
+        if (from_number in characters.keys() and "name" in characters.from_number.keys()):
+            characters[from_number]["name"] = request.values.get('Body', None)
+            resp = MessagingResponse().message("Hi " + characters[from_number]["name"])
+            counter = 0
+            for k,v in characters:
+                if ("name" in v.keys()):
+                    counter += 1
+            if (counter >= len(characters)):
+                game_sequence_1()
+        elif (from_number not in characters.keys()):
+            characters[from_number] = {}
+            resp = MessagingResponse().message("Thanks for joining the game player number " + str(player_count+1) + " Please send your name.")
 
-        player_count += 1
-        if (player_count == max_characters):
-            print "character limit reached"
-            stage = 1
-            game_sequence_1()
+            player_count += 1
+            if (player_count == max_characters):
+                print "character limit reached"
+        else:
+            resp = MessagingResponse().message("Please wait until next stage, waiting for all characters to join")
 
         return str(resp)
     elif (stage==1): #game is ON - receving name
-        from_number = request.values.get('From', None)
-        name = request.values.get('Body', None)
-        characters[from_number]["name"] = name
-        resp = MessagingResponse().message("You are " + name)
+        # from_number = request.values.get('From', None)
+        # name = request.values.get('Body', None)
+        # characters[from_number]["name"] = name
+        resp = MessagingResponse().message("thanks for favourite color")
         return resp
 
 def send_delayed_text():
     time.sleep(10)
     for k in characters.keys():
         message = client.api.account.messages.create(to=k, from_=twilio_number, body="this is text number 1")
-
+    time.sleep(20)
+    for k in characters.keys():
+        message = client.api.account.messages.create(to=k, from_=twilio_number, body="favourite color?")
+    stage = 1
 
 def game_sequence_1():
     for k in characters.keys():
