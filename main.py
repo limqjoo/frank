3a3fc83cceea2a6c39da9471f8918c5533eda9bc
Tom Chambers <tom.chambers@gmail.com>
2017-04-22 16:46:08 +0100

    start game

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request
 from twilio.twiml.voice_response import VoiceResponse
 from twilio.rest import Client
 from twilio.twiml.messaging_response import MessagingResponse
@@ -6,8 +6,6 @@ from twilio.twiml.messaging_response import MessagingResponse
 account_sid = "AC1236670c86e97c36e0e77f6622ae9fb9"
 auth_token = "5cb698f28a9fdc1dbbf147e95ef430b8"
 client = Client(account_sid, auth_token)
-
-app = Flask(__name__)
 
 stage = 0
 player_count = 0
@@ -24,6 +22,8 @@ characters = {
     }
 }
 
+app = Flask(__name__)
+
 # endpoint that receives texts
     # if game waiting (stage==0)
         # takes a phone number - if numbers == 2, start game
@@ -34,13 +34,17 @@ characters = {
 @app.route("/sms", methods=['GET', 'POST'])
 def sms_receiver():
     """responds to text messages from players"""
+    global stage
+    global player_count
 
     if (stage==0):
-        characters[player_count][number] = from_number
+        from_number = request.values.get('From', None)
+        characters[player_count]["number"] = from_number
         player_count += 1
-        resp = MessagingResponse().message("Thanks for joining the game, your character name is " + characters[player_count][name])
+        resp = MessagingResponse().message("Thanks for joining the game, your character name is " + characters[player_count]["name"])
         return str(resp)
         if (player_count == len(characters)):
+
             stage = 1
             start_game()
 
@@ -51,9 +55,9 @@ def sms_receiver():
 
 def start_game():
     pass
-        """ message = client.api.account.messages.create(to="+12316851234",
-                                                 from_="+15555555555",
-                                                 body="Hello there!")"""
+    """ message = client.api.account.messages.create(to="+12316851234",
+                                             from_="+15555555555",
+                                             body="Hello there!")"""
 
 
 @app.route("/voice", methods=['GET', 'POST'])
