381decd52d3605a0678e24b78194fd6f6c33ed5f
Tom Chambers <tom.chambers@gmail.com>
2017-04-22 16:17:08 +0100

    setup reply sms

--- main.py
+++ main.py
@@ -1,23 +1,69 @@
 from flask import Flask
 from twilio.twiml.voice_response import VoiceResponse
+from twilio.rest import Client
+
+account_sid = "AC1236670c86e97c36e0e77f6622ae9fb9"
+auth_token = "5cb698f28a9fdc1dbbf147e95ef430b8"
+client = Client(account_sid, auth_token)
 
 app = Flask(__name__)
 
+stage = 0
+characters = {
+    0: {
+        "name": "Charlie",
+        "number": 0,
+        "description": None,
+    },
+    1: {
+        "name": "Lisa",
+        "number": 0,
+        "description": None,
+    }
+}
+
 # endpoint that receives texts
-    # if game started
-        # do game logic
-    # if game waiting
+    # if game waiting (stage==0)
         # takes a phone number - if numbers == 2, start game
+    # if waiting for description (stage==1)
+        # takes a description - if numbers == 2, start game
+    # wait for code word at end of story (stage==2)
+        # send success/fail texts based on test
+@app.route("/sms", methods=['GET', 'POST'])
+def sms_receiver():
+    """responds to text messages from players"""
+
+    resp = MessagingResponse().message("Hello, Mobile Monkey")
+    return str(resp)
+
+    """ message = client.api.account.messages.create(to="+12316851234",
+                                             from_="+15555555555",
+                                             body="Hello there!")"""
+
+
+@app.route("/voice", methods=['GET', 'POST'])
+def hello_monkey():
+    """Respond to incoming requests."""
+    resp = VoiceResponse()
+    resp.say("Hello Tom. Your first clue is just around the corner. Walk a few meters and you shall see some trees. Press the key corresponding to the number of trees that you have seen. You're getting close. Be careful. Big sister is watching you!")
+
+    return str(resp)
 
 # game start
     # assign characters randomly
-    # send welcome text
+    # send welcome text - ask for description of person
+
+# game stage 2
     # wait 30 seconds
     # send out exposition texts - send a text each to gf and mum
     # send a location of the library
     # wait 2 minutes
     # send, sorry cant come text, wait for other person
-    #
+    # wait 3 minutes for the two people to talk
+    # ask for the code word
+
+# game stage 3
+    # on code word received, test it and send success/fail message
 
 
 @app.route("/", methods=['GET', 'POST'])
